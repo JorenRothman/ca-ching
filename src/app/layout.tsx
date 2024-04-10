300ffe28a12fb577d2fa6c66e2837315c11b1590
Joren <rothmanjoren@gmail.com>
2024-04-10 20:42:03 +0200

    Setup env variables

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,18 +2,18 @@ import type { Metadata } from "next";
 import "./globals.css";
 
 export const metadata: Metadata = {
-  title: "Ca Ching",
-  description: "A simple budgeting app",
+    title: "Ca Ching",
+    description: "A simple budgeting app",
 };
 
 export default function RootLayout({
-  children,
+    children,
 }: Readonly<{
-  children: React.ReactNode;
+    children: React.ReactNode;
 }>) {
-  return (
-    <html lang="en">
-      <body>{children}</body>
-    </html>
-  );
+    return (
+        <html lang="en">
+            <body>{children}</body>
+        </html>
+    );
 }
