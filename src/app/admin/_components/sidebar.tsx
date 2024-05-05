6f52b72a282372c5dfa7970ba9f383bcf925c69a
Joren Rothman <joren@instance.studio>
2024-05-05 14:35:45 +0200

    Updated sidebar

--- src/app/admin/_components/sidebar.tsx
+++ src/app/admin/_components/sidebar.tsx
@@ -74,6 +74,10 @@ export default function Sidebar() {
                         </TooltipTrigger>
                         <TooltipContent side="right">Clients</TooltipContent>
                     </Tooltip>
+                </TooltipProvider>
+            </nav>
+            <nav className="mt-auto flex flex-col items-center gap-4 px-2 sm:py-5">
+                <TooltipProvider>
                     <Tooltip>
                         <TooltipTrigger asChild>
                             <Link
@@ -93,22 +97,6 @@ export default function Sidebar() {
                         <TooltipContent side="right">
                             Access Tokens
                         </TooltipContent>
-                    </Tooltip>
-                </TooltipProvider>
-            </nav>
-            <nav className="mt-auto flex flex-col items-center gap-4 px-2 sm:py-5">
-                <TooltipProvider>
-                    <Tooltip>
-                        <TooltipTrigger asChild>
-                            <Link
-                                href="#"
-                                className="flex h-9 w-9 items-center justify-center rounded-lg text-muted-foreground transition-colors hover:text-foreground md:h-8 md:w-8"
-                            >
-                                <Settings className="h-5 w-5" />
-                                <span className="sr-only">Settings</span>
-                            </Link>
-                        </TooltipTrigger>
-                        <TooltipContent side="right">Settings</TooltipContent>
                     </Tooltip>
                 </TooltipProvider>
             </nav>
