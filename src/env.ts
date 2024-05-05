7c2edefc253f8551667a8b8f9b2c7955d2216df1
Joren Rothman <joren@instance.studio>
2024-05-05 13:53:41 +0200

    Removed env package

--- src/env.ts
+++ src/env.ts
@@ -1,14 +1,29 @@
-import { createEnv } from "@t3-oss/env-nextjs";
-import { z } from "zod";
+import { ZodError, z } from "zod";
 
-export const env = createEnv({
-    server: {
-        NODE_ENV: z.string().default("production"),
-        DATABASE_URL: z.string(),
-        GITHUB_CLIENT_ID: z.string(),
-        GITHUB_CLIENT_SECRET: z.string(),
-        SECRET: z.string().min(16),
-    },
-    client: {},
-    experimental__runtimeEnv: {},
+export const envSchema = z.object({
+    NODE_ENV: z.string().default("development"),
+    DATABASE_URL: z.string(),
+    GITHUB_CLIENT_ID: z.string(),
+    GITHUB_CLIENT_SECRET: z.string(),
+    SECRET: z.string().min(16),
 });
+
+export type EnvSchema = z.infer<typeof envSchema>;
+
+try {
+    envSchema.parse(process.env);
+} catch (error) {
+    if (error instanceof ZodError) {
+        let message = "Missing required values in .env:\n";
+        error.issues.forEach((issue) => {
+            message += issue.path[0] + "\n";
+        });
+        const e = new Error(message);
+        e.stack = "";
+        throw e;
+    } else {
+        console.error(error);
+    }
+}
+
+export const env = envSchema.parse(process.env);
