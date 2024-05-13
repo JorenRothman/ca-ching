1a71f53f6a65fae61225ba312ac68c808dc42a07
Joren <rothmanjoren@gmail.com>
2024-05-13 20:57:11 +0200

    Updated naming

--- src/migrate.ts
+++ src/migrate.ts
@@ -1,11 +1,11 @@
 import "dotenv/config";
 import { migrate } from "drizzle-orm/postgres-js/migrator";
-import { conn, db } from "@/server/db/db";
+import { connection, db } from "@/server/db/db";
 // This will run migrations on the database, skipping the ones already applied
 (async () => {
     await migrate(db, { migrationsFolder: "./drizzle" });
 
-    conn.end();
+    connection.end();
     console.log("migration finished!");
 })();
 
--- src/server/db/db.ts
+++ src/server/db/db.ts
@@ -4,31 +4,21 @@ import postgres from "postgres";
 
 import { env } from "@/env";
 import * as schema from "./schema";
-import { DefaultLogger, type LogWriter } from "drizzle-orm";
-
-class MyLogWriter implements LogWriter {
-    write(message: string) {
-        // console.log(message);
-    }
-}
-
 /**
  * Cache the database connection in development. This avoids creating a new connection on every HMR
  * update.
  */
 const globalForDb = globalThis as unknown as {
-    conn: postgres.Sql | undefined;
+    connection: postgres.Sql | undefined;
 };
 
-export const conn = globalForDb.conn ?? postgres(env.DATABASE_URL);
+export const connection = globalForDb.connection ?? postgres(env.DATABASE_URL);
 
 if (env.NODE_ENV !== "production") {
-    globalForDb.conn = conn;
+    globalForDb.connection = connection;
 }
 
-const logger = new DefaultLogger({ writer: new MyLogWriter() });
-
-export const db = drizzle(conn, { schema, logger });
+export const db = drizzle(connection, { schema });
 
 export const adapter = new DrizzlePostgreSQLAdapter(
     db,
